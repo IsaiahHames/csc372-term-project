474429c1c90ab89eacfedd9eaae2227a088cd31d
IsaiahHames <isaiah.hames14@gmail.com>
2026-05-05 18:23:34 -0400

    cors fix

--- express-backend/server.js
+++ express-backend/server.js
@@ -9,12 +9,15 @@ require("./auth/passport.js");
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true
-}));
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 
 app.use(multer().none());
